c315646d73923084d20941a6a1a0ee079bc162e9
caste <castelan.15@outlook.es>
2023-10-05 01:53:39 -0600

    Menu Flotante

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Menu_Contextual_Flotante"
+include(":app")
+ 
